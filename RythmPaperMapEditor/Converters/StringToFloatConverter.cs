8f663edca06d3ded86a4366171dc82aa91b07106
sharphurt <pavellukoanov679@gmail.com>
2022-06-28 01:26:45 +0500

    Converters

---
+++ RythmPaperMapEditor/Converters/StringToFloatConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RythmPaperMapEditor.Converters
+{
+    public class StringToFloatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return float.TryParse((string)value, NumberStyles.Any, CultureInfo.InvariantCulture.NumberFormat,
+                out var ret)
+                ? ret
+                : 0;
+        }
+    }
+}
---
+++ RythmPaperMapEditor/Converters/StringToIntConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RythmPaperMapEditor.Converters
+{
+    public class StringToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return int.TryParse((string)value, out var ret) ? ret : 0;
+        }
+    }
+}
